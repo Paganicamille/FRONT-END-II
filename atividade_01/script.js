546a41f779f5b04cc7b6c2594d166477959007d9
Paganicamille <camille.dias@estudante.ifro.edu.br>
2024-09-27 10:06:57 -0400

    first commit

---
+++ atividade_01/script.js
@@ -0,0 +1,5 @@
+const itensMenu = [
+    { texto: 'Home', link: 'index.html' },
+    { texto: 'Sobre', link: 'sobre.html' },
+    { texto: 'Contato', link: 'contato.html' }
+];
---
+++ exemplo_1/script.js
@@ -0,0 +1,27 @@
+const body = document.querySelector('body');
+const p = documente.createElement('p');
+
+body.style.background = '#f567ba';
+
+/*const body = document.querySelector('body');
+const ancora = document.createElement('a');
+ancora.setAttribute('href', 'https://www.ifro.edu.br');
+ancora.setAttribute('target', '_blank');
+ancora.textContent = 'IFRO';
+ancora.style.color = '#f00';
+ancora.style.textDecoration = 'none';
+body.appendChild(ancora);*/
+
+/*const body = document.querySelector('body');
+    const lista = document.createElement ('ul');
+    const item1 = document.creatElement('li');
+    const item2 = document.creatElement('li');
+    const item3 = document.creatElement('li');
+
+    item1.textContent ='item-1';
+    item2.textContent ='item-2';
+    item3.textContent ='item-3';
+
+    lista.append(item1);
+    lista.append(item2);
+    lista.append(item3);*/
